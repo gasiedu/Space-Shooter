f6e5d03b05d7bf7dc38b24409464a843711186eb
George <gasiedu@hotmail.co.uk>
2021-03-22 23:24:35 +0000

    added movement to cube and wraps around border

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -22,12 +22,34 @@ public class Player : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {               //new vector3(-5, 0, 0) * 5 * real time
+    {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-
         transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
         transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 3.8f, 0);
+        }
+        else if (transform.position.y < 3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
+
+        if (transform.position.x > 11.9f)
+        {
+            transform.position = new Vector3(-11.9f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -11.9f)
+        {
+            transform.position = new Vector3(11.9f, transform.position.y, 0);
+        }
     }
 }
