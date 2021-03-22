ccc4f6a67d25f54df9acc54262a3180822013231
George <gasiedu@hotmail.co.uk>
2021-03-22 00:15:50 +0000

    player cube move script updated with wasd

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //public or private referance
+    //data type (int, float, bool, strings)
+    //everu vaiable has a name
+    //optional value assigned
+
+    private float _speed = 5.5f;
+
+    public float horizontalInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +22,12 @@ public class Player : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {               //new vector3(-5, 0, 0) * 5 * real time
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+
+        transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+        transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
     }
 }
